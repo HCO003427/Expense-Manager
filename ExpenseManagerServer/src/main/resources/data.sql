3516379f600ae995fba8d5027a5d4fbf8405be6b
Chetan Kukreja <kukreja.chetan@yahoo.com>
2019-01-06 22:45:45 +0530

    Adding Springboot files

---
+++ ExpenseManagerServer/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into employees values (101,'ITA','chetan');
+insert into employees values (102,'System Engineer','Abhishek');
+insert into employees values (103,'AST','Vinoth');
+insert into employees values (104,'ASOC','Sooraj');
+insert into employees values (105,'Consultant','Karthik');
