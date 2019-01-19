f1ea74f7f8cbc9b7de4e01d6368b2353ffc178fd
Chetan Kukreja <kukreja.chetan@yahoo.com>
2019-01-19 22:39:47 +0530

    testing for polling

--- ExpenseManagerServer/src/main/java/com/expensemanager/controllers/UserController.java
+++ ExpenseManagerServer/src/main/java/com/expensemanager/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.expensemanager.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.expensemanager.models.User;
 import com.expensemanager.repos.UserRepo;
