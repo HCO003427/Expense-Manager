6b328e53c587103bf4f4654cbf421e2d81eebae4
Chetan Kukreja <kukreja.chetan@yahoo.com>
2019-01-24 00:11:23 +0530

    reverting to old version

--- ExpenseManagerServer/src/main/java/com/expensemanager/controllers/UserController.java
+++ ExpenseManagerServer/src/main/java/com/expensemanager/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.expensemanager.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.expensemanager.models.User;
 import com.expensemanager.repos.UserRepo;
@@ -30,7 +33,6 @@ public class UserController {
 	
 	@GetMapping(path="/api/users")
 	public List<User> getAllUsers(){
-		System.out.println("Request to fetch all users");
 		return repo.findAll();
 	}
 	
